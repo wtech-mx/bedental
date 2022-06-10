ac525b4e750dd4cb07d982020c8988104a02eb30
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2022-06-10 00:07:45 -0500

    Mail::failures()

--- app/Http/Controllers/SendEmailController.php
+++ app/Http/Controllers/SendEmailController.php
@@ -22,7 +22,11 @@ class SendEmailController extends Controller
 
         Mail::to('adrianwebtech@gmail.com')->send(new NotifyMail($mailData));
 
-
+      if (Mail::failures()) {
+           return response()->Fail('Sorry! Please try again latter');
+      }else{
+           return response()->success('Great! Successfully send in your mail');
+         }
 
     }
 }
