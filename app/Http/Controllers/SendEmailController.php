4206c5e74c9275cfb32f7ce1d439ee6f2cf98906
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2022-06-09 23:30:29 -0500

    Pruebas

--- app/Http/Controllers/SendEmailController.php
+++ app/Http/Controllers/SendEmailController.php
@@ -14,21 +14,8 @@ class SendEmailController extends Controller
 
     public function index()
     {
-        $emailSubject = 'Envio de Ticket';
-        $emailBody = 'Detalles del Ticket  KLINMEXICO';
 
-        //usar para un solo correo de destino
-        $emaifor = "adrianwebtech@gmail.com";
-        //usar para varios  correos de destino
-
-        $arrayEmails = ['dinopiza@yahoo.com.mx','adrianwebtech@gmail.com'];
-
-
-        Mail::send('emails.demoMail',['msg' => $emailBody], function($message) use($emailSubject,$arrayEmails){
-            $message->from("contacto_webtech@yahoo.com.mx","KlinMexico");
-            $message->subject($emailSubject);
-            $message->to($arrayEmails);
-        });
+      Mail::to('adrianwebtech@gmail.com')->send('dinopiza@yahoo.com.mx');
 
       if (Mail::failures()) {
            return response()->Fail('Sorry! Please try again latter');
