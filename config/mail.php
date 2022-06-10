6ce9343ea9458bbd9844f6598fc39a5d172cb667
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2022-06-09 23:46:46 -0500

    from

--- config/mail.php
+++ config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'dinopiza@yahoo.com.mx'),
+        'name' => env('MAIL_FROM_NAME', 'Webtech'),
     ],
 
     /*
