6d11efb1397d3729333e948b27a3ffab7748bb32
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2022-06-15 14:15:41 -0500

    Composer quite livewire

--- resources/views/clients/modal_facturas.blade.php
+++ resources/views/clients/modal_facturas.blade.php
@@ -55,7 +55,7 @@
                     </div>
 
                     <div class="tab-pane fade" id="masiva{{ $row->id }}" role="tabpanel" aria-labelledby="masiva-tab">
-    
+
                         @if (!empty($facturas))
                         @foreach ($facturas as $fact)
                             @if ($fact->id_clients == $row->id)
