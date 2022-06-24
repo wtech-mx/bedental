327f8b49d936331a46ca28bc1f8372c0fd71e8d3
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2022-06-24 00:32:21 -0500

    Dinmic perfil and change img profile

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -60,7 +60,7 @@
               <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <div class="media align-items-center">
                   <span class="avatar avatar-sm rounded-circle">
-                    <img alt="Image placeholder" src="{{ asset('assets/img/theme/team-4.jpg')}}">
+                    <img alt="Image placeholder" src="{{ asset('assets/img/brand/cropped-LOGOS-BEDTL-1.png')}}">
                   </span>
                   <div class="media-body ml-2 d-none d-lg-block">
                     <span class="mb-0 text-sm  font-weight-bold">{{ Auth::user()->name }}</span>
@@ -69,15 +69,11 @@
               </a>
               <div class="dropdown-menu dropdown-menu-right">
                 <div class="dropdown-header noti-title">
-                  <h6 class="text-overflow m-0">Welcome!</h6>
+                  <h6 class="text-overflow m-0">Bienvenido!</h6>
                 </div>
-                <a href="#!" class="dropdown-item">
+                <a href="{{ route('users.edit',Auth::user()->id) }}" class="dropdown-item">
                   <i class="ni ni-single-02"></i>
-                  <span>My profile</span>
-                </a>
-                <a href="#!" class="dropdown-item">
-                  <i class="ni ni-settings-gear-65"></i>
-                  <span>Settings</span>
+                  <span>Mi perfil</span>
                 </a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
