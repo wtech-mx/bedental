1b44075233f458f2d2676d48d0224b101fc9e565
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2022-06-26 19:53:43 -0500

    Actualizacion de cambio de colores en toos events

--- app/Http/Controllers/ColoresController.php
+++ app/Http/Controllers/ColoresController.php
@@ -11,12 +11,6 @@ class ColoresController extends Controller
 {
     public function update_colores(Request $request)
     {
-
-        $colores = Colores::get()->count();
-
-        $servicio = $request->servicio;
-        $color = $request->color;
-
         foreach ($request->get('color') as $key => $value){
             $color = Colores::find($request->get('id')[$key]);
             $color -> servicio = $request->get('servicio')[$key];
@@ -24,7 +18,26 @@ class ColoresController extends Controller
             $color->update();
         }
 
+           $alertas = Alertas::get()->pluck('id_color');
+           $colores = Colores::get()->pluck('id');
 
+           $comps = Alertas::join('colores','alertas.id_color','=','colores.id')
+               ->select('colores.color')->get();
+
+           $comps2 = Alertas::select('id')->get();
+
+           foreach ($comps2 as $comp2){
+
+                $comps = Alertas::join('colores','alertas.id_color','=','colores.id')
+               ->select('colores.color')->get();
+
+                foreach ($comps as $comp){
+                    $alertas = Alertas::find($comp2->id);
+                    $alertas->color = $comp->color;
+                    $alertas->update();
+                }
+            }
+           
 
         Session::flash('edit', 'Se ha editado sus datos con exito');
         return redirect()->back();
